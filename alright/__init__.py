11759841c9381baabc2e1be113cd08352da3d098
João Paulo Gasperi <jpgasperi1@gmail.com>
2025-10-03 15:12:33 -0300

    Updated default time_out

--- alright/__init__.py
+++ alright/__init__.py
@@ -28,7 +28,7 @@ LOGGER = logging.getLogger()
 
 
 class WhatsApp(object):
-    def __init__(self, browser=None, time_out=600):
+    def __init__(self, browser=None, time_out=60):
         # CJM - 20220419: Added time_out=600 to allow the call with less than 600 sec timeout
         # web.open(f"https://web.whatsapp.com/send?phone={phone_no}&text={quote(message)}")
 
